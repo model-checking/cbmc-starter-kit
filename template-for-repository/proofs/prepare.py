1f92ae462b598f7cb58175cf1e0d3294c9bd5df3
Kareem Khazem <karkhaz@amazon.com>
2020-07-10 18:14:07 +0100

    Add license header for prepare.py

--- template-for-repository/proofs/prepare.py
+++ template-for-repository/proofs/prepare.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not
+# use this file except in compliance with the License. A copy of the License is
+# located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
 
 """Prepare the source tree for proofs in continuous integration."""
+
 
 import os
 import subprocess
 
+
 MAKEFILE = "Makefile"
 CBMC_BATCH_YAML = "cbmc-batch.yaml"
+
 
 def create_cbmc_batch_yaml(folder):
     """Run make to create cbmc-batch.yaml in folder."""
@@ -28,6 +45,7 @@ def create_cbmc_batch_yaml(folder):
                                   ' '.join(error.cmd),
                                   error.stderr.strip())) from None
 
+
 def create_cbmc_batch_yaml_files(root='.'):
     """Create cbmc-batch.yaml in all directories under root."""
 
@@ -35,10 +53,12 @@ def create_cbmc_batch_yaml_files(root='.'):
         if CBMC_BATCH_YAML in files and MAKEFILE in files:
             create_cbmc_batch_yaml(folder)
 
+
 def prepare():
     """Prepare the source tree for proofs in continuous integration."""
 
     create_cbmc_batch_yaml_files()
+
 
 if __name__ == "__main__":
     prepare()
