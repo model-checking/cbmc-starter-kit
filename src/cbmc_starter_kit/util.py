1e0577c52dff2095b77041c5d3657711a1f33d0f
Mark R. Tuttle <mrtuttle@amazon.com>
2022-04-08 17:37:33 -0400

    Rewrite setup scripts to copy and not link.
    This is a nearly total rewrite of setup.py, setup_proof.py and
    util.py.  First, they are written to copy template files into place
    instead of symlinking to them in the submodule.  Second, they are
    rewritten to use the GitPython and Path modules.  Both changes
    resulted in much less, much simpler code.

--- src/cbmc_starter_kit/util.py
+++ src/cbmc_starter_kit/util.py
@@ -4,39 +4,62 @@
 """Methods of manipulating the templates repository."""
 
 from pathlib import Path
-import logging
-import os
-import shutil
+import importlib
 
 from cbmc_starter_kit import repository
 
 REPOSITORY_TEMPLATES = "template-for-repository"
 PROOF_TEMPLATES = "template-for-proof"
-
 PROOF_DIR = "proofs"
-
-# There are some files that we should copy to the project repository rather than
-# symlinking. This is because users are expected to modify these files. If the
-# files were symlinks, then modifying them would dirty up this submodule, which
-# would prevent project owners from cleanly updating it.
-COPY_INSTEAD = [
-    "Makefile-project-defines",
-    "Makefile-project-targets",
-    "Makefile-project-testing",
-    ".gitignore",
-]
 
 ################################################################
 
-def script_dir():
-    """Directory containing setup scripts."""
+def package_root():
+    """Directory containing the package."""
 
-    return os.path.dirname(os.path.abspath(__file__))
+    # There are so many incompatible, deprecated ways of doing this
+    # available in Python version 3.6 through 3.10, and the right
+    # solution is in question.
+    #
+    # The right solution is actually
+    #
+    #   importlib.resources.files(__package__)
+    #
+    # but importlib.resources appears in 3.7 (with a backport
+    # importlib_resources available before 3.7) and files itself
+    # appears in 3.9
+    #
+    # The next solution is
+    #
+    #   importlib.resources.path(__package__, resource_name)
+    #
+    # but the resource_name must be a string and can't be a directory
+    # or a path, and this deprecated after 3.9 in favor of files().
+    #
+    # The next solution is
+    #
+    #   pkgutil.get_data(__package__, resource_path)
+    #
+    # but this returns contents and not a path.  But, amazing, this
+    # works even if the package is an egg or a zipfile!
+    #
+    # The next solution is
+    #
+    #     pkg_resources.resource_filename(__package__, resource_path)
+    #
+    # but pkg_resources is slow and distributed with setuptools and
+    # not a standard module.
 
-def templates_root():
-    """Directory containing the AWS-templates-for-CBMC repository."""
+    # this gives the path to the package __init__.py
+    init = importlib.util.find_spec(__package__).origin
+    # this gives the path to the package directory
+    return Path(init).parent
 
-    return os.path.dirname(script_dir())
+def repository_template_root():
+    return package_root() / REPOSITORY_TEMPLATES
+
+def proof_template_root():
+    return package_root() / PROOF_TEMPLATES
 
 ################################################################
 # Ask the user for set up information
@@ -82,68 +105,3 @@ def ask_for_source_file(func, cwd=None, repo=None):
     return src
 
 ################################################################
-
-def files_under_root(root):
-    """The list of files in the filesystem under root."""
-
-    cwd = os.getcwd()
-    try:
-        os.chdir(root)
-        return [os.path.join(path, name)
-                for path, _, files in os.walk('.') for name in files]
-    finally:
-        os.chdir(cwd)
-
-
-def link_files(name, src, dst):
-    """Link file dst/name to file src/name, return number skipped"""
-
-    src_name = os.path.normpath(os.path.join(src, name))
-    dst_name = os.path.normpath(os.path.join(dst, name))
-
-    os.makedirs(os.path.dirname(dst_name), exist_ok=True)
-    src_link = os.path.relpath(src_name, os.path.dirname(dst_name))
-
-    if os.path.basename(name) in COPY_INSTEAD:
-        install_method = ("copy", shutil.copyfile)
-        src_link = src_name
-    else:
-        install_method = ("symlink", os.symlink)
-
-    if os.path.exists(dst_name):
-        logging.warning("Skipping %s %s -> %s: file exists",
-                        install_method[0], name, src_link)
-        return 1
-
-    logging.debug(
-        "Creating %s %s -> %s", install_method[0], name, src_link)
-    install_method[1](src_link, dst_name)
-    return 0
-
-def copy_directory_contents(src, dst, exclude=None):
-    """Link the contents of one directory into another."""
-
-    src = os.path.normpath(src)
-    dst = os.path.normpath(dst)
-
-    assert os.path.isdir(src)
-    assert os.path.isdir(dst)
-
-    skipped = 0
-    for name in files_under_root(src):
-        name = os.path.normpath(name)
-        if exclude and name.startswith(exclude):
-            continue
-        skipped += link_files(name, src, dst)
-
-    if skipped:
-        logging.warning("To overwrite a skipped file, "
-                        "delete the file and rerun the script.")
-
-def copy_repository_templates(cbmc_root):
-    """Copy the files in the repository template into the CBMC root."""
-
-    copy_directory_contents(os.path.join(templates_root(),
-                                         REPOSITORY_TEMPLATES),
-                            cbmc_root,
-                            exclude="negative_tests")
