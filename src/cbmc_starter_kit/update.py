baa10d39cea9ba8e3f2d6385021a7286b8c65259
Mark R. Tuttle <mrtuttle@amazon.com>
2022-04-13 11:47:01 -0400

    Modify handling of starter kit and litani submodule removal.
    Add flag --remove-starter-kit-submodule.  Check for installation of
    starter kit submodule.  Remove it if flag is given and recommend
    removal otherwise.
    
    Add flag --remove-litani-submodule.  Check for installation of litani
    submodule and litani command.  Remove submodule if both exist, and
    update makefiles to use the command; and recommend removal otherwise.
    Recommend installation of litani command if submodule exists and
    command does not.
    
    Also clean up the parser descriptions of all command line arguments.

--- src/cbmc_starter_kit/update.py
+++ src/cbmc_starter_kit/update.py
@@ -7,6 +7,7 @@
 
 from pathlib import Path
 import logging
+import platform
 import shutil
 import subprocess
 import sys
@@ -30,34 +31,37 @@ def parse_arguments():
         {'flag': '--starter-kit-root',
          'type': Path,
          'metavar': 'STARTER_KIT',
-         'help': 'Root of CBMC starter kit submodule if it exists (default: installed submodule)'},
+         'help': """
+          Root of CBMC starter kit submodule (default: None or root of starter kit submodule
+          installed in repository containing CBMC)"""},
         {'flag': '--no-migrate',
          'action': 'store_true',
          'help': """
-          Do not remove symlinks under CBMC_ROOT.
-          Normally remove symlinks under CBMC_ROOT to files under STARTER_KIT_ROOT."""},
+          Do not remove symlinks under CBMC.
+          Normally remove symlinks under CBMC to files under STARTER_KIT."""},
         {'flag': '--no-test-removal',
          'action': 'store_true',
          'help': """
-          Do not remove negative tests in CBMC_ROOT/negative_tests.
-          Normally remove the directory CBMC_ROOT/negative_tests since most projects don't
+          Do not remove negative tests in CBMC/negative_tests.
+          Normally remove the directory CBMC/negative_tests since most projects don't
           use these tests."""},
         {'flag': '--no-update',
          'action': 'store_true',
          'help': """
-          Do not update Makefile.common and run-cbmc-proofs.py under CBMC_ROOT/proofs.
-          Normally update these files with copies in this version of the current starter kit."""},
-        {'flag': '--no-starter-kit-removal',
+          Do not update Makefile.common and run-cbmc-proofs.py under CBMC/proofs.
+          Normally update these files with the versions in the starter kit package."""},
+        {'flag': '--remove-starter-kit-submodule',
          'action': 'store_true',
          'help': """
-          Do not try to remove the CBMC starter kit submodule.
-          Normally check for existence of submodule but remove only with --force."""},
-        {'flag': '--force',
+          Remove the starter kit submodule if it is present.
+          Normally just recommend removal."""},
+        {'flag': '--remove-litani-submodule',
          'action': 'store_true',
          'help': """
-          Remove the CBMC starter kit submodule if it exists.
-          This positive confirmation is a safety check that is required before removing
-          any repository submodules."""},
+          Remove the litani submodule and update the definition of LITANI in
+          Makefile-template-defines if the litani submodule is present and
+          the litani command is in PATH.
+          Normally just recommend removal."""},
     ]
     args = arguments.create_parser(options, desc).parse_args()
     arguments.configure_logging(args)
@@ -117,6 +121,44 @@ def files_under_root(root=None, symlinks_only=False):
 
 ################################################################
 
+def remove_submodule(cbmc_root, submodule_name, submodule_path):
+    """Remove the submodule at the named path in the repository"""
+
+    for submodule in git.Repo(cbmc_root, search_parent_directories=True).submodules:
+        if submodule.path == str(submodule_path):
+            logging.warning('Removing: %s submodule: %s', submodule_name, submodule_path)
+            try:
+                submodule.remove()
+            except git.InvalidGitRepositoryError as error:
+                # remove uses git cherry to compare working branches with upstream branches
+                # and throws InvalidGitRepositoryError if it finds an inconsistency.
+                logging.debug(error)
+                logging.error('Unable to remove %s submodule: %s',  submodule_name, submodule_path)
+                logging.error('Try again after running "git fetch" in %s', submodule_path)
+            return
+    logging.debug("Failed to remove %s submodule: %s", submodule_name, submodule_path)
+
+def update_litani_makefile_variable(cbmc_root, path):
+    """Update LITANI to LITANI?=litani in the makefile at the named path"""
+
+    with open(cbmc_root/path, encoding='utf-8') as makefile:
+        lines = makefile.read().splitlines()
+    litani_prefix = 'LITANI ?='
+    if not any(line.strip().startswith(litani_prefix) for line in lines):
+        logging.debug("Not updating LITANI in makefile: %s", path)
+        return
+
+    litani_define = f'{litani_prefix} litani' # litani_define == 'LITANI ?= litani'
+    logging.warning("Updating '%s' in makefile: %s", litani_define, path)
+    with open(cbmc_root/path, 'w', encoding='utf-8') as makefile:
+        for line in lines:
+            if line.strip().startswith(litani_prefix):
+                line = litani_define
+            makefile.write(f"{line}\n")
+    return
+
+################################################################
+
 def migrate(cbmc_root, starter_kit_root):
     logging.debug('Migrating CBMC starter kit')
     templates = files_under_root(starter_kit_root/util.REPOSITORY_TEMPLATES)
@@ -149,41 +191,46 @@ def update(cbmc_root, quiet=False):
         (logging.debug if quiet else logging.warning)('Copying: %s -> %s', src, dst)
         version.copy_with_version(src, dst)
 
-def remove_submodule(cbmc_root, submodule_name, submodule_path, force=False):
-    logging.debug('Checking for %s submodule', submodule_name)
-    if not submodule_path:
-        logging.debug('%s submodule does not exist', submodule_name)
+def check_for_starter_kit_submodule(cbmc_root, remove=False):
+    starter_path = repository.starter_kit_root(repo=cbmc_root, abspath=False)
+    if not starter_path:
+        logging.debug("Found starter kit submodule is not installed")
         return
-    if not force:
-        logging.warning('Use --force if you want to remove the %s submodule: %s',
-                        submodule_name, submodule_path)
+
+    if not remove:
+        logging.warning("Use --remove-starter-kit-submodule to remove the starter kit submodule")
         return
-    for submodule in git.Repo(cbmc_root, search_parent_directories=True).submodules:
-        if submodule.path == str(submodule_path):
-            logging.warning('Removing: %s submodule: %s', submodule_name, submodule_path)
-            try:
-                submodule.remove()
-            except git.InvalidGitRepositoryError as error:
-                # remove uses git cherry to compare working branches with upstream branches
-                # and throws InvalidGitRepositoryError if it finds an inconsistency.
-                logging.debug(error)
-                logging.error('Unable to remove %s submodule: %s',  submodule_name, submodule_path)
-                logging.error('Try again after running "git fetch" in %s', submodule_path)
-            return
-    logging.critical('This should be impossible: Failed to remove %s submodule: %s',
-                     submodule_name, submodule_path)
-    sys.exit(1)
 
+    remove_submodule(cbmc_root, "starter kit", starter_path)
 
-def remove_starter_kit_submodule(cbmc_root, force=False):
-    starter_kit = repository.starter_kit_root(repo=cbmc_root, abspath=False)
-    remove_submodule(cbmc_root, "starter kit", starter_kit, force)
+def check_for_litani_submodule(cbmc_root, remove=False):
+    litani_path = repository.litani_root(repo=cbmc_root, abspath=False)
+    if not litani_path:
+        logging.debug("Found litani submodule is not installed")
+        return
 
-def remove_litani_submodule(cbmc_root, force=False):
-    # Warning: Removing the litani submodule may also require changing
-    # the definition of LITANI in Makefile-template-defines
-    litani = repository.litani_root(repo=cbmc_root, abspath=False)
-    remove_submodule(cbmc_root, "litani", litani, force)
+    litani_command = shutil.which('litani')
+    if not litani_command:
+        logging.debug("Found litani command is not installed")
+        system = platform.system()
+        if system == "Darwin":
+            logging.warning("Consider replacing the litani submodule with the litani command "
+                            "available via 'brew install litani'")
+        if system == "Linux":
+            logging.warning("Consider replacing the litani submodule with the litani command "
+                            "available via 'apt install litani*.deb'")
+            logging.warning("Get the litani package litani*.deb from the release page "
+                            "https://github.com/awslabs/aws-build-accumulator/releases/latest")
+        return
+
+    if not remove:
+        logging.warning("Use --remove-litani-submodule to remove the litani submodule "
+                        "and update makefiles to use the litani command")
+        return
+
+    remove_submodule(cbmc_root, "litani", litani_path)
+    update_litani_makefile_variable(cbmc_root, Path(util.PROOF_DIR)/util.TEMPLATE_DEFINES)
+    update_litani_makefile_variable(cbmc_root, Path(util.PROOF_DIR)/util.PROJECT_DEFINES)
 
 ################################################################
 
@@ -202,8 +249,8 @@ def main():
         remove_negative_tests(args.cbmc_root)
     if not args.no_update:
         update(args.cbmc_root)
-    if not args.no_starter_kit_removal:
-        remove_starter_kit_submodule(args.cbmc_root, args.force)
+    check_for_starter_kit_submodule(args.cbmc_root, args.remove_starter_kit_submodule)
+    check_for_litani_submodule(args.cbmc_root, args.remove_litani_submodule)
 
 ################################################################
 
